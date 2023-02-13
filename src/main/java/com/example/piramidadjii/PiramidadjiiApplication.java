7833187133609f049642852473948c76223bd275
Teodor <teodor.kuzev@nevexis.com>
2023-02-13 16:40:54 +0200

    asd

---
+++ src/main/java/com/example/piramidadjii/PiramidadjiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.piramidadjii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramidadjiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramidadjiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/piramidadjii/PiramidadjiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piramidadjii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiramidadjiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
