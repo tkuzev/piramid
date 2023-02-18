bc391a723d29934711f2d43d55d46989406fe792
Lora Stefanova <lora.stefanova99@gmail.com>
2023-02-18 16:02:20 +0200

    da si otvorite jirata

--- src/main/java/com/example/piramidadjii/services/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/piramidadjii/services/impl/TransactionServiceImpl.java
@@ -17,12 +17,10 @@ import java.util.stream.Stream;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
-
     private Long FLAT_PERCENTAGE = 5L;
 
     @Autowired
     private TransactionRepository transactionRepository;
-
     @Autowired
     private SubscriptionPlanServiceImpl subscriptionPlanService;
 
@@ -30,39 +28,37 @@ public class TransactionServiceImpl implements TransactionService {
     @Transactional
     public void createTransaction(Person person, BigDecimal price) {
         final Long[] percent = new Long[1];
-        AtomicInteger counter = new AtomicInteger();
+        AtomicInteger counter = new AtomicInteger(0);
 
         traverseFromNodeToRoot(person)
-                .parallel()
-                .limit(5)
+                // removed .parallel() shtoto se nasira
+                .limit(5) // one more transaction for the root node must be added nqkoga
                 .forEach(node -> setNewTransactions(person, price, percent, counter, node));
     }
 
     private void setNewTransactions(Person person, BigDecimal price, Long[] percent, AtomicInteger counter, Person node) {
-        List<Long> percents = subscriptionPlanService.mapFromStringToLong(person.getSubscriptionPlan().getPercents());
-        checkPercent(person, percent, counter, node,percents);
+        checkPercent(person, percent, counter, node);
         counter.getAndAdd(1);
         returnNewValue(node, price, percent[0]);
     }
-    private void checkPercent(Person person, Long[] percent, AtomicInteger counter, Person node,List<Long> percents) {
 
-        if (counter.get()>percents.size()){
-            return;
-        }
+    private void checkPercent(Person person, Long[] percent, AtomicInteger counter, Person node) {
+        List<Long> percents = subscriptionPlanService.mapFromStringToLong(person.getSubscriptionPlan().getPercents());
 
         if (node.getId().equals(person.getId())) {
-                percent[0] = FLAT_PERCENTAGE;
+            percent[0] = FLAT_PERCENTAGE;
         } else if (counter.get() > percents.size()) {
             percent[0] = 0L;
         } else {
-            percent[0] = percents.get(counter.get());
+            percent[0] = percents.get(counter.get() - 1);
         }
     }
+
     public Stream<Person> traverseFromNodeToRoot(Person node) {
-        //
         if (/* Stop */ Objects.isNull(node) || /* skip company */ Objects.isNull(node.getParent())) {
             return Stream.empty();
         }
+
         return Stream.concat(Stream.of(node), traverseFromNodeToRoot(node.getParent()));
     }
 
@@ -71,11 +67,9 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     public BigDecimal returnNewValue(Person person, BigDecimal sellingPrice, Long percent) {
-
         person.setBalance(person.getBalance().add(sellingPrice.multiply(BigDecimal.valueOf(percent))));
         transactionDetails(person, sellingPrice, percent);
         return person.getBalance();
-
     }
 
     private void transactionDetails(Person person, BigDecimal price, Long percent) {
@@ -85,6 +79,4 @@ public class TransactionServiceImpl implements TransactionService {
         transaction.setPersonId(person.getId());
         transactionRepository.save(transaction);
     }
-
-
 }
