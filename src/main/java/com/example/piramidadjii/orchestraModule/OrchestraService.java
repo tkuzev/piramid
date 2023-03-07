b2a2fadec44e1567f569dad7e07ff01dedfa458c
Konstantin Georgiev <92094692+Konstantin204@users.noreply.github.com>
2023-03-07 13:17:57 +0200

    pak orchestra prerabotvame

--- src/main/java/com/example/piramidadjii/orchestraModule/OrchestraService.java
+++ src/main/java/com/example/piramidadjii/orchestraModule/OrchestraService.java
@@ -1,9 +1,5 @@
 package com.example.piramidadjii.orchestraModule;
 
-import com.example.piramidadjii.binaryTreeModule.entities.BinaryPerson;
-import com.example.piramidadjii.registrationTreeModule.entities.RegistrationPerson;
-
 public interface OrchestraService {
-    BinaryPerson registerNewPerson(RegistrationPerson person, boolean preferredDirection);
 
 }
