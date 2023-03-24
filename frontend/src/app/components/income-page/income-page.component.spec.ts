83461a040218b0ffd22bb5e5167099a99cc85b5e
Konstantin Georgiev <92094692+Konstantin204@users.noreply.github.com>
2023-03-24 15:05:02 +0200

    icon home button

---
+++ frontend/src/app/components/income-page/income-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IncomePageComponent } from './income-page.component';
+
+describe('IncomePageComponent', () => {
+  let component: IncomePageComponent;
+  let fixture: ComponentFixture<IncomePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IncomePageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IncomePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/income-page/income-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-page',
+  templateUrl: './income-page.component.html',
+  styleUrls: ['./income-page.component.css']
+})
+export class IncomePageComponent {
+
+}
