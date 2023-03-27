3071a2ae062ee27fa5851a23e58655a3910473d6
Konstantin Georgiev <92094692+Konstantin204@users.noreply.github.com>
2023-03-27 16:08:51 +0300

    registration form

--- frontend/src/app/components/registration-form/registration-form.component.ts
+++ frontend/src/app/components/registration-form/registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration-form',
