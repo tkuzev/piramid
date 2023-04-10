3f0f70caf4c79838d23a93cc225909968ca0e22a
Lora <lora.stefanova99@gmail.com>
2023-04-10 15:26:20 +0300

    planove pravq

--- frontend/src/app/components/subscription-plans/subscription-plans.component.ts
+++ frontend/src/app/components/subscription-plans/subscription-plans.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./subscription-plans.component.css']
 })
 export class SubscriptionPlansComponent {
-
+  name: string = 'Platinum';
+  flatPercent: number = 5;
+  price: number = 500;
+  level1: number = 5;
+  level2: number = 4;
+  level3: number = 3;
+  level4: number = 2;
+  isEligibleForBinary: boolean = true;
 }
